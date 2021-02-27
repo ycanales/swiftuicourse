bc32479b85ff0f73b32c5b1e7daf10b9683d27d2
Cristian Yanez <cristian.yanez.canales@gmail.com>
2021-02-27 17:09:19 -0300

    Adds card design with illustration to ContentView

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+        
+            Text("SwiftUI for iOS 14")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                
+            Text("20 Sections")
+                .font(.footnote)
+                .foregroundColor(Color.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/))
+                .preferredColorScheme(.dark)
+        }
     }
 }
