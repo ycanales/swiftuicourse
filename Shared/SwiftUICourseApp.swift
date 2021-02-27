b179593b648a0b2e13541090a363bf4044038c4d
Cristian Yanez <cristian.yanez.canales@gmail.com>
2021-02-27 15:29:11 -0300

    Initial Commit

---
+++ Shared/SwiftUICourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICourseApp.swift
+//  Shared
+//
+//  Created by Cristian Yáñez on 27-02-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
