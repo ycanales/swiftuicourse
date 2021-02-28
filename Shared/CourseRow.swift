80aa252a82f9b6aa507dd6d123dba9aa17572cf3
Cristian Yanez <cristian.yanez.canales@gmail.com>
2021-02-28 00:14:25 -0300

    Completes SF Symbols and List Styles lesson

--- Shared/CourseRow.swift
+++ Shared/CourseRow.swift
@@ -11,8 +11,12 @@ struct CourseRow: View {
     var body: some View {
         HStack(alignment: .top) {
             Image(systemName: "paperplane.circle.fill")
-                .renderingMode(.original)
-                .imageScale(.large)
+                .renderingMode(.template)
+                .frame(width: 48.0, height: 48.0)
+                .imageScale(.medium)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
             VStack(alignment: .leading, spacing: 4.0) {
                 Text("SwiftUI")
                     .font(.subheadline)
@@ -21,12 +25,16 @@ struct CourseRow: View {
                     .font(.footnote)
                     .foregroundColor(.secondary)
             }
+            Spacer()
         }
     }
 }
 
 struct CourseRow_Previews: PreviewProvider {
     static var previews: some View {
-        CourseRow()
+        Group {
+            CourseRow()
+            CourseRow()
+        }
     }
 }
---
+++ Shared/CoursesView.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesView.swift
+//  SwiftUICourse
+//
+//  Created by Cristian Yáñez on 28-02-21.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
